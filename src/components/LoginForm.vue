0820f36e441f893334fd19e49db6c5dd78021376
Max Rozvora <m.rozvora@viseven.com>
2022-01-16 11:31:33 +0200

    create project structure

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+      <div class="bg-white py-6 rounded-md px-10 max-w-lg shadow-md">
+        <h1 class="text-center text-lg font-bold text-gray-500">Login Form</h1>
+        <div class="space-y-4 mt-6">
+          <div class="w-full">
+            <input
+              type="text"
+              placeholder="email"
+              class="px-4 py-2 bg-gray-50"
+            />
+          </div>
+          <div class="w-full">
+            <input
+              type="text"
+              placeholder="password"
+              class="px-4 py-2 bg-gray-50"
+            />
+          </div>
+        </div>
+        <button
+          class="w-full mt-5 bg-indigo-600 text-white py-2 rounded-md font-semibold tracking-tight"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Search.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>search page</h1>
+</template>
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style scoped></style>
